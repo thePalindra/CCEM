7ac8e1146bd6f7c63d9fc7b5a2acd3b705b535de
Palindra <rjp.goncalves@campus.fct.unl.pt>
2023-06-15 11:20:30 +0100

    Spring Boot backend application starter
    Created a Spring Boot application. It runs using jdk 17 because jdk 11 was bothering me with errors. 
    
    The application has 3 dependencies:
    - Spring boot starter web
    - Spring boot tests
    - Spring boot JPA

---
+++ ccem/src/main/java/pt/CCEM/ccem/CcemApplication.java
@@ -0,0 +1,14 @@
+package pt.CCEM.ccem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class CcemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcemApplication.class, args);
+	}
+
+}
---
+++ ccem/src/test/java/pt/CCEM/ccem/CcemApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.CCEM.ccem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
