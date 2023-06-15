e936d755955413440451cf23a66a131396831679
Palindra <rjp.goncalves@campus.fct.unl.pt>
2023-06-15 18:20:17 +0100

    GSON import and service update for testing
    Just some code for testing.

--- ccem/src/main/java/pt/CCEM/ccem/events/EventsController.java
+++ ccem/src/main/java/pt/CCEM/ccem/events/EventsController.java
@@ -1,5 +1,6 @@
 package pt.CCEM.ccem.events;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,33 +9,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/events")
 public class EventsController implements EventsAPI{
 
+    @Autowired
+    private EventsService eventsService;
+
     @Override
     public ResponseEntity<String> getAll(Integer page) {
-        return null;
+        return this.eventsService.getAll(page);
     }
 
     @Override
     public ResponseEntity<String> getById(Long id) {
-        return null;
+        return this.eventsService.getById(id);
     }
 
     @Override
     public ResponseEntity<String> getAllInRegion(Long id, Integer page) {
-        return null;
+        return this.eventsService.getAllInRegion(id, page);
     }
 
     @Override
     public ResponseEntity<String> getAllInCounty(Long id, Integer page) {
-        return null;
+        return this.eventsService.getAllInCounty(id, page);
     }
 
     @Override
     public ResponseEntity<String> put(Long id) {
-        return null;
+        return this.eventsService.put(id);
     }
 
     @Override
     public ResponseEntity<String> postOne() {
-        return null;
+        return this.eventsService.postOne();
     }
 }
--- ccem/src/main/java/pt/CCEM/ccem/events/EventsService.java
+++ ccem/src/main/java/pt/CCEM/ccem/events/EventsService.java
@@ -1,7 +1,66 @@
 package pt.CCEM.ccem.events;
 
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import pt.CCEM.ccem.data.County;
+import pt.CCEM.ccem.data.Event;
+import pt.CCEM.ccem.data.Region;
+import pt.CCEM.ccem.data.Space;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 @Service
 public class EventsService {
+
+    private static final Gson gson = new Gson();
+
+    private Map<Long, Event> events;
+    private Map<Long, Space> spaces;
+    private Map<Long, County> counties;
+    private Map<Long, Region> regions;
+
+    public EventsService() {
+        this.events = new TreeMap<>();
+        this.spaces = new TreeMap<>();
+        this.counties = new TreeMap<>();
+        this.regions = new TreeMap<>();
+    }
+
+    public ResponseEntity<String> getAll(Integer page) {
+        return null;
+    }
+
+    public ResponseEntity<String> getById(Long id) {
+        return null;
+    }
+
+    public ResponseEntity<String> getAllInRegion(Long id, Integer page) {
+        return null;
+    }
+
+    public ResponseEntity<String> getAllInCounty(Long id, Integer page) {
+        return null;
+    }
+
+    public ResponseEntity<String> put(Long id) {
+        return null;
+    }
+
+    public ResponseEntity<String> postOne() {
+        try {
+            Event event = Event.builder()
+                    .id(1l)
+                    .build();
+            this.events.put(1l, event);
+
+            return new ResponseEntity<>(gson.toJson(this.events), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(gson.toJson(e), HttpStatus.BAD_REQUEST);
+        }
+    }
 }
